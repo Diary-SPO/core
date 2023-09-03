7150a8f2f2115471dd874638d7d4e265d549b132
Semyon Okulov <khidey@inbox.ru>
2023-09-03 12:43:46 +0700

    WIP

--- backend/index.ts
+++ backend/index.ts
@@ -1,88 +1,88 @@
-import express, { Request, Response } from 'express';
-import rateLimit from 'express-rate-limit';
-import cors from 'cors';
-import axios, {Axios, AxiosError} from 'axios'; // Импортируем Axios
-import 'dotenv/config';
+import express, { type Request, type Response } from 'express'
+import rateLimit from 'express-rate-limit'
+import cors from 'cors'
+import axios, { Axios, AxiosError } from 'axios'
+import 'dotenv/config'
 
-const apiUrl = process.env.SERVER_URL;
-const app = express();
-const port = process.env.PORT || 3000;
+const apiUrl = process.env.SERVER_URL
+const app = express()
+const port = process.env.PORT || 3000
 
 app.use(cors({
   origin: '*'
-}));
+}))
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,
   max: 80,
   message: 'Превышено ограничение запросов в минуту'
-});
+})
 
-app.use(limiter);
+app.use(limiter)
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello');
-});
+  res.send('Hello')
+})
 
 app.get('/lessons/:id/:startDate/:endDate', async (req: Request, res: Response) => {
   try {
-    const secret = req.headers.secret;
-    
+    const secret = req.headers.secret
+
     if (!secret || typeof secret !== 'string') {
-      res.json('Something is wrong').status(500);
-      return;
+      res.json('Something is wrong').status(500)
+      return
     }
-    
-    const { startDate, endDate, id } = req.params;
-    let formattedStartDate, formattedEndDate;
-    
+
+    const { startDate, endDate, id } = req.params
+    let formattedStartDate, formattedEndDate
+
     if (!id) {
-      res.json('Something is wrong').status(500);
+      res.json('Something is wrong').status(500)
     }
-    
+
     if (startDate && endDate) {
-      formattedStartDate = startDate.toString();
-      formattedEndDate = endDate.toString();
-      
-      const startTimestamp = new Date(startDate).getTime();
-      const endTimestamp = new Date(endDate).getTime();
-      const differenceInDays = (endTimestamp - startTimestamp) / (1000 * 3600 * 24);
-      
+      formattedStartDate = startDate.toString()
+      formattedEndDate = endDate.toString()
+
+      const startTimestamp = new Date(startDate).getTime()
+      const endTimestamp = new Date(endDate).getTime()
+      const differenceInDays = (endTimestamp - startTimestamp) / (1000 * 3600 * 24)
+
       if (differenceInDays > 14) {
-        const newEndDate = new Date(startTimestamp + 14 * 24 * 60 * 60 * 1000);
-        formattedEndDate = newEndDate.toISOString().substring(0, 10);
+        const newEndDate = new Date(startTimestamp + 14 * 24 * 60 * 60 * 1000)
+        formattedEndDate = newEndDate.toISOString().substring(0, 10)
       }
     } else {
-      const currentDate = new Date();
-      formattedStartDate = currentDate.toISOString().substring(0, 10);
-      
-      const endDate = new Date(currentDate.getTime() + 14 * 24 * 60 * 60 * 1000);
-      formattedEndDate = endDate.toISOString().substring(0, 10);
+      const currentDate = new Date()
+      formattedStartDate = currentDate.toISOString().substring(0, 10)
+
+      const endDate = new Date(currentDate.getTime() + 14 * 24 * 60 * 60 * 1000)
+      formattedEndDate = endDate.toISOString().substring(0, 10)
     }
-    
+
     const response = await axios.get(`${apiUrl}/students/${id}/lessons/${formattedStartDate}/${formattedEndDate}`, {
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
         Cookie: secret
       }
-    });
-    
-    const data = response.data;
-    res.json(data);
+    })
+
+    const data = response.data
+    res.json(data)
   } catch (e) {
-    console.error('/lessons/:id/:startDate/:endDate', e);
-    res.status(500).json(`Internal server error: ${e}`);
+    console.error('/lessons/:id/:startDate/:endDate', e)
+    res.status(500).json(`Internal server error: ${e}`)
   }
-});
+})
 
 app.post('/login', async (req: Request, res: Response) => {
-  const login = req.headers.login;
-  const password = req.headers.password;
-  
+  const login = req.headers.login
+  const password = req.headers.password
+
   if (!login || !password || typeof login !== 'string' || typeof password !== 'string') {
-    return res.status(400).json('Invalid login or password');
+    return res.status(400).json('Invalid login or password')
   }
-  
+
   try {
     const response = await axios.post(`${apiUrl}/security/login`, {
       login,
@@ -92,32 +92,32 @@ app.post('/login', async (req: Request, res: Response) => {
       headers: {
         'Content-Type': 'application/json;charset=UTF-8'
       }
-    });
-    
-    const data = response.data;
-    
-    const setCookieHeader = response.headers['set-cookie'];
-    const cookieString = Array.isArray(setCookieHeader) ? setCookieHeader.join('; ') : setCookieHeader;
-    
-    return res.json({ data, cookie: cookieString }).status(response.status);
+    })
+
+    const data = response.data
+
+    const setCookieHeader = response.headers['set-cookie']
+    const cookieString = Array.isArray(setCookieHeader) ? setCookieHeader.join('; ') : setCookieHeader
+
+    return res.json({ data, cookie: cookieString }).status(response.status)
   } catch (error) {
     if (error instanceof AxiosError) {
-      const axiosError = error as AxiosError;
-      
+      const axiosError = error as AxiosError
+
       if (axiosError.response) {
-        const { status, data } = axiosError.response;
-        return res.status(status).json(data);
+        const { status, data } = axiosError.response
+        return res.status(status).json(data)
       } else {
-        console.error('/login', axiosError);
-        return res.status(500).json(`Internal server error: ${axiosError.message}`);
+        console.error('/login', axiosError)
+        return res.status(500).json(`Internal server error: ${axiosError.message}`)
       }
     } else {
-      console.error('/login', error);
-      return res.status(500).json(`Internal server error: ${error}`);
+      console.error('/login', error)
+      return res.status(500).json(`Internal server error: ${error}`)
     }
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
--- frontend/src/views/LoginForm.tsx
+++ frontend/src/views/LoginForm.tsx
@@ -7,12 +7,12 @@ import {
 import { useActiveVkuiLocation, useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
 import Hashes from 'jshashes';
 import bridge from '@vkontakte/vk-bridge';
+import { Icon28ErrorCircleOutline } from '@vkontakte/icons';
 
 import { AuthData } from '../../../shared';
 import { VIEW_SCHEDULE } from '../routes';
 
 import PanelHeaderWithBack from '../components/PanelHeaderWithBack';
-import {Icon28ErrorCircleOutline} from "@vkontakte/icons";
 
 const LoginForm: FC<{ id: string }> = ({ id }) => {
   const { panel: activePanel, panelsHistory } = useActiveVkuiLocation();
@@ -52,12 +52,12 @@ const LoginForm: FC<{ id: string }> = ({ id }) => {
       setIsDataInvalid(true);
       return;
     }
-    
+
     const passwordHashed = (new Hashes.SHA256()).b64(password);
-    
+
     setIsLoading(true);
     setPopout(<ScreenSpinner state='loading' />);
-    
+
     const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/login`, {
       method: 'POST',
       headers: {
@@ -68,7 +68,7 @@ const LoginForm: FC<{ id: string }> = ({ id }) => {
     });
 
     if (response.status === 401) {
-      console.log('401')
+      console.log('401');
       setIsLoading(false);
       setErrorScreenSpinner();
       setIsDataInvalid(true);
@@ -78,20 +78,20 @@ const LoginForm: FC<{ id: string }> = ({ id }) => {
       setErrorScreenSpinner();
       throw new Error(`Failed to fetch login / status: ${response.status} / statusText: ${response.statusText}`);
     }
-    
+
     const dataResp = await response.json() as AuthData;
     if (!Array.isArray(dataResp)) {
       setPopout(
         <Snackbar
           onClose={() => setPopout(null)}
-          before={<Icon28ErrorCircleOutline fill="var(--vkui--color_icon_negative)" />}
-          subtitle={`Попробуйте заного или сообщите об ошибке`}
+          before={<Icon28ErrorCircleOutline fill='var(--vkui--color_icon_negative)' />}
+          subtitle='Попробуйте заного или сообщите об ошибке'
         >
           Ошибка при попытке авторизации
-        </Snackbar>
-      )
+        </Snackbar>,
+      );
     }
-    
+
     try {
       await bridge.send('VKWebAppStorageSet', {
         key: 'cookie',
