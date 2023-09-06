caf3c9828bf8e9c43d1f3394948b84b7b71abed9
Semyon Okulov <khidey@inbox.ru>
2023-09-06 18:26:02 +0700

    improve cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,38 +1,42 @@
-import express from 'express'
-import cors from 'cors'
-import rateLimit from 'express-rate-limit'
-import 'dotenv/config'
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import 'dotenv/config';
 
-import { preventCrossSiteScripting } from './src/middleware'
-
-import helmet from 'helmet'
+import { corsMiddleware, preventCrossSiteScripting } from './src/middleware';
+import helmet from 'helmet';
 
 import {
   dashboard, helloRoute, lessonsRoute, loginRoute, performanceCurrent
-} from './src/routes'
+} from './src/routes';
 
-const app = express()
-const port = process.env.PORT ?? 3000
+const app = express();
+const port = process.env.PORT ?? 3000;
 
-app.use(preventCrossSiteScripting)
-app.use(helmet())
-app.use(cors({ origin: '*' }))
-app.use(express.json())
+app.use(preventCrossSiteScripting);
+app.use(helmet());
+
+const FIFTEEN_MINS_IN_MS = 900000;
+
+app.use(express.json());
 
 const limiter = rateLimit({
-  windowMs: 60 * 1000,
-  max: 40,
-  message: 'Превышено ограничение запросов в минуту'
-})
+  windowMs: FIFTEEN_MINS_IN_MS,
+  max: 30,
+  statusCode: 200,
+  message: 'ОК',
+});
 
-app.use(limiter)
+app.use(limiter);
 
-app.use('/', helloRoute)
-app.use('/lessons', lessonsRoute)
-app.use('/login', loginRoute)
-app.use('/dashboard', dashboard)
-app.use('/performance.current', performanceCurrent)
+app.use(corsMiddleware);
+
+app.use('/', helloRoute);
+app.use('/lessons', lessonsRoute);
+app.use('/login', loginRoute);
+app.use('/dashboard', dashboard);
+app.use('/performance.current', performanceCurrent);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
-})
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
--- backend/src/middleware/index.ts
+++ backend/src/middleware/index.ts
@@ -1,26 +1,55 @@
-import { type NextFunction, type Request, type Response } from 'express'
+import { Request, Response, NextFunction } from 'express';
+import cors, { CorsOptions } from 'cors';
 
-export const preventCrossSiteScripting = (req: Request, res: Response, next: NextFunction): void => {
-  res.setHeader('X-XSS-Protection', '1; mode=block')
-  next()
-}
+export const preventCrossSiteScripting = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  next();
+};
 
-export const checkId = (req: Request, res: Response, next: NextFunction): any => {
-  const { id } = req.params
-
+export const checkId = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  const { id } = req.params;
+  
   if (!id) {
-    return res.status(400).json('Something is bad #2')
+    return res.status(400).json('Something is bad #2');
   }
+  
+  next();
+};
 
-  next()
-}
-
-export const checkCookie = (req: Request, res: Response, next: NextFunction): any => {
-  const secret = req.headers.secret
-
-  if (!secret || typeof secret !== 'string') {
-    return res.status(400).json('Something is bad #1')
+export const checkCookie = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  const secret = req.headers.secret as string | undefined;
+  
+  if (!secret || typeof secret !== 'string' || secret !== '') {
+    return res.status(400).json('Something is bad #1');
   }
+  
+  next();
+};
 
-  next()
-}
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    const allowedOriginPattern = /^https:\/\/[a-z0-9-]+\.pages\.vk-apps\.com$/;
+    
+    console.info('origin', origin)
+    
+    if (allowedOriginPattern.test(origin as string) || origin === 'https://localhost:5173') {
+      callback(null, true);
+    } else {
+      callback('CORS' as unknown as Error, false);
+    }
+  },
+};
+
+export const corsMiddleware = cors(corsOptions);
