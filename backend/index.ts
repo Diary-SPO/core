e0df1c6ebebeac02c0f0a578fbbdbc36e4569f41
Semyon Okulov <khidey@inbox.ru>
2023-09-06 23:49:14 +0700

    little fixes

--- backend/index.ts
+++ backend/index.ts
@@ -1,68 +1,46 @@
-import express from 'express';
-import rateLimit from 'express-rate-limit';
-import helmet from 'helmet';
-import cors from 'cors';
-import 'dotenv/config';
+import express from 'express'
+import rateLimit from 'express-rate-limit'
+import helmet from 'helmet'
+import cors from 'cors'
+import 'dotenv/config'
 
-import { preventCrossSiteScripting } from './src/middleware';
+import { corsMiddleware, preventCrossSiteScripting } from './src/middleware'
 
 import {
   dashboard, helloRoute, lessonsRoute, loginRoute, performanceCurrent
-} from './src/routes';
+} from './src/routes'
 
-const app = express();
-const port = process.env.PORT ?? 3000;
+const app = express()
+const port = process.env.PORT ?? 3000
 
-app.use(preventCrossSiteScripting);
-app.use(helmet());
+app.use(preventCrossSiteScripting)
+app.use(helmet())
 
-const FIFTEEN_MINS_IN_MS = 900000;
+const FIFTEEN_MINS_IN_MS = 900000
 
-app.use(express.json());
+app.use(express.json())
 
 const limiter = rateLimit({
   windowMs: FIFTEEN_MINS_IN_MS,
   max: 50,
   statusCode: 200,
-  message: 'ОК',
-});
-
-const hosts = ['localhost', 'https://prod-app51743817'];
-
-app.use(limiter);
+  message: 'ОК'
+})
 
 app.use(cors({
   origin: '*',
-  optionsSuccessStatus: 204,
-}));
+  optionsSuccessStatus: 204
+}))
 
-app.use((req, res, next) => {
-  res.set({
-    'Access-Control-Allow-Origin': 'https://prod-app51743817-951dbcc22b92.pages-ac.vk-apps.com',
-    'Access-Control-Allow-Methods': 'GET, POST',
-    'Access-Control-Allow-Headers': 'X-Requested-With'
-  })
-  const resHost = req.get('Origin') || req.get('Host') || null;
-  console.log(resHost);
-  let   isCORS = true;
-  if(resHost != null)
-  for(let i = 0; i < hosts.length; i++) {
-    if (resHost.indexOf(hosts[i]) >= 0) {
-      isCORS = false;
-      break;
-    }
-  }
-  if (!isCORS) next();
-  else         res.status(200).send('CORS');
-});
+app.use(limiter)
+app.use(corsMiddleware)
 
-app.use('/', helloRoute);
-app.use('/lessons', lessonsRoute);
-app.use('/login', loginRoute);
-app.use('/dashboard', dashboard);
-app.use('/performance.current', performanceCurrent);
+app.use('/', helloRoute)
+app.use('/lessons', lessonsRoute)
+app.use('/login', loginRoute)
+app.use('/dashboard', dashboard)
+app.use('/performance.current', performanceCurrent)
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
-
+  console.log(`Server is running at http://localhost:${port}`)
+})
--- backend/src/middleware/index.ts
+++ backend/src/middleware/index.ts
@@ -1,39 +1,67 @@
-import { Request, Response, NextFunction } from 'express';
+import { type Request, type Response, type NextFunction } from 'express'
 
 export const preventCrossSiteScripting = (
   req: Request,
   res: Response,
   next: NextFunction
 ): void => {
-  res.setHeader('X-XSS-Protection', '1; mode=block');
-  next();
-};
+  res.setHeader('X-XSS-Protection', '1; mode=block')
+  next()
+}
 
 export const checkId = (
   req: Request,
   res: Response,
   next: NextFunction
 ): any => {
-  const { id } = req.params;
-  
+  const { id } = req.params
+
   if (!id) {
-    return res.status(400).json('Something is bad #2');
+    return res.status(400).json('Something is bad #2')
   }
-  
-  next();
-};
+
+  next()
+}
 
 export const checkCookie = (
   req: Request,
   res: Response,
   next: NextFunction
 ): any => {
-  const secret = req.headers.secret as string | undefined;
+  const secret = req.headers.secret as string | undefined
 
   if (!secret || typeof secret !== 'string' || secret === '') {
-    return res.status(400).json('Something is bad #1');
+    return res.status(400).json('Something is bad #1')
   }
-  
-  next();
-};
 
+  next()
+}
+
+const allowedHosts = ['localhost', 'https://prod-app51740302']
+
+const isHostAllowed = (host: string): boolean => {
+  return allowedHosts.some(allowedHost => host.includes(allowedHost))
+}
+
+export const corsMiddleware = (req: Request, res: Response, next: NextFunction): void => {
+  res.set({
+    'Access-Control-Allow-Origin': 'https://stage-app51740302-f7d9a1dae661.pages.vk-apps.com',
+    'Access-Control-Allow-Methods': 'GET, POST',
+    'Access-Control-Allow-Headers': 'X-Requested-With'
+  })
+
+  const resHost = req.get('Origin') || req.get('Host') || null
+  console.log(resHost)
+
+  let isCORS = true
+
+  if (resHost != null) {
+    isCORS = !isHostAllowed(resHost)
+  }
+
+  if (isCORS) {
+    res.status(200).send('OK')
+  } else {
+    next()
+  }
+}
