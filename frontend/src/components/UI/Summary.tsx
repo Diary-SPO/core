9d5771be128d8067b34e2e570d436d067da305aa
Semyon Okulov <khidey@inbox.ru>
2023-09-14 13:04:13 +0700

    refactoring

--- frontend/src/components/UI/Summary.tsx
+++ frontend/src/components/UI/Summary.tsx
@@ -1,56 +1,51 @@
 import { FC } from 'react';
 import {
-  Div, Group, Header, MiniInfoCell,
+  Group, Header, MiniInfoCell,
 } from '@vkontakte/vkui';
 import { Icon20EducationOutline, Icon28BrainOutline } from '@vkontakte/icons';
 import Mark from './Mark';
+import { TMark } from '../../types';
 
-const Summary: FC = () => {
-  const getItemFromLocalStorage = (key: string) => {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : null;
-  };
+interface ISummary {
+  totalNumberOfMarks: string | null;
+  averageMark: string | null;
+  markCounts: Record<number, number> | null;
+}
 
-  const totalNumberOfMarks = getItemFromLocalStorage('totalNumberOfMarks');
-  const averageMark = getItemFromLocalStorage('averageMark');
-  const markCounts = getItemFromLocalStorage('markCounts');
-
-  return (
-    <Group header={<Header mode='tertiary'>Статистика</Header>}>
-      <Div>
+const Summary: FC<ISummary> = ({ markCounts, totalNumberOfMarks, averageMark }) => (
+  <Group header={<Header mode='tertiary'>Статистика</Header>}>
+    <MiniInfoCell
+      before={<Icon20EducationOutline style={{ marginTop: 4 }} />}
+      after={<Mark size='s' mark={totalNumberOfMarks as TMark} />}
+    >
+      Суммарное количество оценок:
+    </MiniInfoCell>
+    <MiniInfoCell
+      before={<Icon28BrainOutline style={{ marginTop: 4 }} width={20} height={20} />}
+      after={<Mark size='s' mark={averageMark as TMark} />}
+    >
+      Общий средний балл:
+    </MiniInfoCell>
+    {markCounts && (
+    <div style={{
+      display: 'flex', flexDirection: 'row-reverse', justifyContent: 'space-around', gap: 5,
+    }}
+    >
+      {[2, 3, 4, 5].map((mark) => (
+        markCounts[mark] > 0 && (
         <MiniInfoCell
-          before={<Icon20EducationOutline style={{ marginTop: 4 }} />}
-          after={<Mark size='s' mark={totalNumberOfMarks} />}
+          key={mark}
+          before={<Mark mark={mark} size='s' />}
         >
-          Суммарное количество оценок:
+          x
+          {' '}
+          {markCounts[mark]}
         </MiniInfoCell>
-        <MiniInfoCell
-          before={<Icon28BrainOutline style={{ marginTop: 4 }} width={20} height={20} />}
-          after={<Mark size='s' mark={averageMark} />}
-        >
-          Общий средний балл:
-        </MiniInfoCell>
-      </Div>
-      {markCounts && (
-        <div style={{
-          display: 'flex', flexDirection: 'row-reverse', justifyContent: 'space-around', gap: 5,
-        }}
-        >
-          {[2, 3, 4, 5].map((mark) => (
-            markCounts[mark] > 0 && (
-              <MiniInfoCell
-                before={<Mark mark={mark} size='s' />}
-              >
-                x
-                {' '}
-                {markCounts[mark]}
-              </MiniInfoCell>
-            )
-          ))}
-        </div>
-      )}
-    </Group>
-  );
-};
+        )
+      ))}
+    </div>
+    )}
+  </Group>
+);
 
 export default Summary;
