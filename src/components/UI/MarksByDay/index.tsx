75338c258b90627ac06650355f291247430756af
Semyon Okulov <khidey@inbox.ru>
2023-09-16 18:44:22 +0700

    fix marks

--- src/components/UI/MarksByDay/index.tsx
+++ src/components/UI/MarksByDay/index.tsx
@@ -19,47 +19,50 @@ const truncateString = (str: string, maxLength: number) => {
 };
 
 interface IMarksByDay {
-  [key: string] : {
-    grades: Grade[];
-    lessonName: string
-  }
+  [key: string]: {
+    [lessonName: string]: Grade[];
+  };
 }
 
 const MarksByDay: FC<IPerformanceCurrent> = ({ performanceData }) => {
   const marksByDay: IMarksByDay = {};
-
+  
   performanceData?.daysWithMarksForSubject?.forEach((subject) => {
     subject?.daysWithMarks?.forEach((dayWithMarks) => {
       const day = new Date(dayWithMarks.day).toLocaleDateString();
       const grades = dayWithMarks.markValues.map((gradeText) => Grade[gradeText]);
       const lessonName = subject.subjectName;
-
+      
       if (grades.length > 0 && grades.every((grade) => !Number.isNaN(parseFloat(grade as string)))) {
         if (!marksByDay[day]) {
-          marksByDay[day] = { grades: [], lessonName: '' };
+          marksByDay[day] = {};
         }
-
+        
+        if (!marksByDay[day][lessonName]) {
+          marksByDay[day][lessonName] = [];
+        }
+        
         // @ts-ignore
-        marksByDay[day].grades = [...marksByDay[day].grades, ...grades];
-        marksByDay[day].lessonName = lessonName;
+        marksByDay[day][lessonName] = [...marksByDay[day][lessonName], ...grades];
       }
     });
-  });
-
+  })
+  
   const formatDate = (dateString: string) => {
     const parts = dateString.split('.');
     return new Date(Number(parts[2]), Number(parts[1]) - 1, Number(parts[0]));
   };
-
+  
   const sortByDay = (marksByDay: IMarksByDay): IMarksByDay => {
     const sortedDays = Object.keys(marksByDay).sort((a, b) => formatDate(b).getTime() - formatDate(a).getTime());
     const marksByDaySort: IMarksByDay = {};
     sortedDays.forEach((day) => {
       marksByDaySort[day] = marksByDay[day];
     });
+
     return marksByDaySort;
   };
-
+  
   return (
     <HorizontalScroll
       showArrows
@@ -68,14 +71,18 @@ const MarksByDay: FC<IPerformanceCurrent> = ({ performanceData }) => {
     >
       <Group header={<Header mode='secondary'>Недавние оценки</Header>}>
         <div className='marksByName'>
-          {Object.entries(sortByDay(marksByDay)).map(([day, { grades, lessonName }]) => (
+          {Object.entries(sortByDay(marksByDay)).map(([day, lessonGrades]) => (
             <div key={day}>
               <Header mode='secondary'>{day}</Header>
               <div style={{ display: 'flex' }}>
-                {grades.map((grade, gradeIndex) => (
-                  <HorizontalCell style={{ maxWidth: 'unset' }} key={`${day}_${gradeIndex}`}>
-                    <Mark style={{ maxWidth: 90 }} mark={grade || 'Н'} size='l' bottom={truncateString(lessonName, 18)} useMargin={false} />
-                  </HorizontalCell>
+                {Object.entries(lessonGrades).map(([lessonName, grades]) => (
+                  <div style={{ display: 'flex' }} key={`${day}_${lessonName}`}>
+                    {grades.map((grade, gradeIndex) => (
+                      <HorizontalCell style={{ maxWidth: 'unset' }} key={`${day}_${lessonName}_${gradeIndex}`}>
+                        <Mark style={{ maxWidth: 90 }} mark={grade || 'Н'} size='l' bottom={truncateString(lessonName, 18)} useMargin={false} />
+                      </HorizontalCell>
+                    ))}
+                  </div>
                 ))}
               </div>
             </div>
