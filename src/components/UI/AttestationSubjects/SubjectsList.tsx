2cc81d4c5649cb2b9dc902e21104aefaf42ce270
Semyon Okulov <khidey@inbox.ru>
2023-11-01 13:27:17 +0700

    sync w/ main & lint

--- src/components/UI/AttestationSubjects/SubjectsList.tsx
+++ src/components/UI/AttestationSubjects/SubjectsList.tsx
@@ -1,7 +1,7 @@
 import { FunctionalComponent } from 'preact'
 import SubjectGroup from './SubjectGroup'
 import Subject from './types'
-import { Div, Group, Header, Spinner } from '@vkontakte/vkui'
+import LoadingData from './LoadingData'
 
 interface ISubjectList {
   semesters: Record<string, Subject[]>
@@ -17,13 +17,7 @@ const SubjectList: FunctionalComponent<ISubjectList> = ({
   isDataLoading,
 }) => (
   <div>
-    {isDataLoading && (
-      <Group header={<Header mode="tertiary">Загрузка...</Header>}>
-        <Div>
-          <Spinner />
-        </Div>
-      </Group>
-    )}
+    {isDataLoading && <LoadingData />}
     {Object.keys(semesters).map((semesterKey) => (
       <SubjectGroup
         key={semesterKey}
@@ -37,94 +31,3 @@ const SubjectList: FunctionalComponent<ISubjectList> = ({
 )
 
 export default SubjectList
-
-//import {
-//   Card,
-//   CardGrid,
-//   Div,
-//   Group,
-//   Header,
-//   InfoRow,
-//   Spinner,
-//   Subhead,
-//   Title,
-// } from '@vkontakte/vkui'
-// import { ExaminationType, Examinations } from 'diary-shared'
-// import { FunctionalComponent } from 'preact'
-//
-// export interface Subject {
-//   id: string
-//   name: string
-//   examinationType: ExaminationType
-//   marks: Record<string, Record<string, string>>
-// }
-//
-// interface SubjectListProps {
-//   semesters?: Record<string, Subject[]>
-//   studentName?: string | null
-//   year?: number | null
-//   isDataLoading: boolean
-// }
-//
-// const SubjectList: FunctionalComponent<SubjectListProps> = ({
-//   semesters,
-//   studentName,
-//   year,
-//   isDataLoading,
-// }) => (
-//   <div>
-//     {isDataLoading && (
-//       <Group header={<Header mode="tertiary">Загрузка...</Header>}>
-//         <Div>
-//           <Spinner />
-//         </Div>
-//       </Group>
-//     )}
-//     {Object.keys(semesters).map((semesterKey) => (
-//       <Group
-//         key={semesterKey}
-//         header={
-//           <Header
-//             style={{ alignItems: 'center' }}
-//             mode="tertiary"
-//             aside={`${studentName}, ${year}`}
-//           >
-//             {semesterKey}
-//           </Header>
-//         }
-//       >
-//         {semesters[semesterKey].map((subject) => (
-//           <CardGrid key={subject.id} size="l">
-//             <Card mode="shadow">
-//               <Div>
-//                 {/*//@ts-ignore типы React не совсем совместимы с Preact*/}
-//                 <Title level="3">{subject.name}</Title>
-//                 <InfoRow header="Тип аттестации">
-//                   {/*//@ts-ignore типы React не совсем совместимы с Preact*/}
-//                   <Subhead>{Examinations[subject.examinationType]}</Subhead>
-//                 </InfoRow>
-//                 <InfoRow header="Оценки">
-//                   {/*//@ts-ignore типы React не совсем совместимы с Preact*/}
-//                   <Subhead>
-//                     {subject.marks[subject.id] &&
-//                     Object.keys(subject.marks[subject.id]).length > 0
-//                       ? Object.keys(subject.marks[subject.id]).map(
-//                           (studentId) => (
-//                             <span key={studentId}>
-//                               {subject.marks[subject.id][studentId]}
-//                             </span>
-//                           )
-//                         )
-//                       : 'Оценок нет'}
-//                   </Subhead>
-//                 </InfoRow>
-//               </Div>
-//             </Card>
-//           </CardGrid>
-//         ))}
-//       </Group>
-//     ))}
-//   </div>
-// )
-//
-// export default SubjectList
