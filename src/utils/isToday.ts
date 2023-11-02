a22625e057e92d875500a35629f2b89ec32a5be2
Semyon Okulov <khidey@inbox.ru>
2023-11-02 13:50:48 +0700

    Пример теста

--- src/utils/isToday.ts
+++ src/utils/isToday.ts
@@ -16,3 +16,22 @@ export const isToday = async (date: Date): Promise<boolean> => {
 
   return wasm.is_today(day, month, year)
 }
+
+/** Пример теста
+ *
+ * describe('isToday test', () => {
+ *   beforeAll(async () => {
+ *     await init(); // Инициализация WASM-модуля
+ *   });
+ *
+ *   it('should return true for today', () => {
+ *     const date = new Date();
+ *     const day = date.getDate();
+ *     const month = date.getMonth();
+ *     const year = date.getFullYear();
+ *
+ *     const result = is_today(day, month, year);
+ *     expect(result).toBe(true);
+ *   });
+ * });
+ */
