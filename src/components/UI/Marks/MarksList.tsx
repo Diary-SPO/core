bfd44ac572ea4d110f17c5c28c51fae8fc70357d
Semyon Okulov <khidey@inbox.ru>
2024-01-11 03:25:07 +0700

    refactor

--- src/components/UI/Marks/MarksList.tsx
+++ src/components/UI/Marks/MarksList.tsx
@@ -7,14 +7,16 @@ interface IMarksList {
   marks: SubjectMarksMap[string]
 }
 
+// TODO: refactor
 const MarksList: FC<IMarksList> = ({ marks }) => (
+  // TODO: перенести в функцию / разделить логику
   <div style={{ display: 'flex' }}>
     {marks.map(({ date, marks: markValues, absenceType }) => (
       <div
         key={`${date}_${absenceType}`}
         style={{ display: 'flex', gap: 5, marginLeft: 10 }}
       >
-        {!absenceType ? (
+        {markValues.length && !absenceType ? (
           markValues.map((mark, k) => (
             <Mark key={k} mark={Grade[mark]} size='s' />
           ))
