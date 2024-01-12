9746ecd83d1326bd6872640ef569b5fc936610f2
Semyon Okulov <khidey@inbox.ru>
2024-01-13 02:01:35 +0700

    refactor: update getLessons

--- src/methods/server/getLessons.ts
+++ src/methods/server/getLessons.ts
@@ -4,22 +4,13 @@ import { ServerResponse } from '../../types'
 import makeRequest from './makeRequest'
 
 export const getLessons = async (
-  startDate?: Date,
-  endDate?: Date
+  startDate: Date,
+  endDate: Date
 ): ServerResponse<Day[]> => {
   const id = localStorage.getItem('id')
 
   if (!id) {
     return 418
-  }
-
-  if (!startDate) {
-    startDate = new Date()
-  }
-
-  if (!endDate) {
-    endDate = new Date()
-    endDate.setDate(endDate.getDate() + 7)
   }
 
   const formattedStartDate = formatDateForRequest(startDate)
