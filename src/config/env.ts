5025c6e3bd95175976c8ccf56035798f70f56c76
Semyon Okulov <khidey@inbox.ru>
2024-01-25 00:10:41 +0700

    wip

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,7 @@
 export const BASE_URL = process.env.PUBLIC_SERVER_URL
 export const SECOND_SERVER_URL = process.env.PUBLIC_SERVER_URL_SECOND
 export const MODE = process.env.PUBLIC_MODE
+export const BETA_VERSION = process.env.PUBLIC_BETA_VERSION
 
 export const Mode = {
   DEV: 'dev',
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import '@vkontakte/vkui/dist/vkui.css'
 import { render } from 'preact'
-
 import AppWrapper from './app/AppWrapper'
+
+import '@vkontakte/vkui/dist/vkui.css'
 import './index.css'
 
 render(<AppWrapper />, document.getElementById('app')!)
