485a8916a104d0c1bff982265fde57e194d27953
scffs <khidey@inbox.ru>
2024-02-12 19:50:47 +0700

    chore: update color

--- apps/web/src/config/colors.ts
+++ apps/web/src/config/colors.ts
@@ -5,7 +5,7 @@ export const VKUI_VIOLET = 'var(--vkui--color_accent_violet)'
 
 export const GREEN = 'linear-gradient(to right, #56ab2f, #a8e063)'
 export const RED = 'linear-gradient(to right, #ff416c, #ff4b2b)'
-export const ORANGE = 'linear-gradient(to right, #f12711, #f5af19)'
+export const ORANGE = 'linear-gradient(to right, #e56d1f, #f5af19)'
 export const VIOLET =
   'linear-gradient(to right, rgb(73, 102, 207), rgb(154 80 255))'
 export const GRAY = '#959595'
