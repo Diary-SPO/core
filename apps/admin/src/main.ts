226726f4946e90d4d0a2dc55d50001b31036eb02
scffs <khidey@inbox.ru>
2024-02-24 01:37:47 +0700

    feat: add ui lib
    Signed-off-by: scffs <khidey@inbox.ru>

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './assets/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -4,12 +4,20 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
 
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 5174,
   },
   plugins: [vue(), vueJsx()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
