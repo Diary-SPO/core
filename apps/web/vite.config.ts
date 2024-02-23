8823ab48283a6992c9498ff22858cf8eceafa557
scffs <khidey@inbox.ru>
2024-02-23 15:31:28 +0700

    fix path
    Signed-off-by: scffs <khidey@inbox.ru>

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: [
       { find: /^@vkontakte\/vkui$/, replacement: '@vkontakte/vkui/dist/cssm' },
       { find: '@utils', replacement: path.resolve(__dirname, './src/utils') },
+      { find: '@types', replacement: path.resolve(__dirname, './src/types') },
       { find: '@config', replacement: path.resolve(__dirname, './src/config') },
       { find: '@store', replacement: path.resolve(__dirname, './src/store') },
       { find: '@hooks', replacement: path.resolve(__dirname, './src/hooks') },
