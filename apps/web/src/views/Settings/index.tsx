5a41406b2abf897220879b4c0c3374736f4696d8
scffs <khidey@inbox.ru>
2024-02-28 07:44:52 +0700

    refactor web pages
    Signed-off-by: scffs <khidey@inbox.ru>

--- apps/web/src/views/Settings/index.tsx
+++ apps/web/src/views/Settings/index.tsx
@@ -1,52 +1,25 @@
-import { PanelHeaderWithBack } from '@components'
-import { Icon28Users } from '@vkontakte/icons'
-import { Avatar, Group, Header, Link, Panel, SimpleCell } from '@vkontakte/vkui'
 import { FunctionalComponent } from 'preact'
-import { diaryAva, winxAva } from '../../images/config.ts'
+import { lazy } from 'preact/compat'
+import { Panel } from '@vkontakte/vkui'
+
+import { PanelHeaderWithBack, Suspense } from '@components'
 import { Props } from '../types.ts'
-import Actions from './Actions'
-import FAQ from './FAQ'
-import Footer from './Footer'
+
+const Contacts = lazy(() => import('./Contacts'))
+const Actions = lazy(() => import('./Actions'))
+const FAQ = lazy(() => import('./FAQ'))
+const Footer = lazy(() => import('./Footer'))
 
 const Settings: FunctionalComponent<Props> = ({ id }) => {
   return (
     <Panel nav={id}>
       <PanelHeaderWithBack title='Настройки' />
-
-      <FAQ />
-
-      <Actions />
-
-      <Group header={<Header mode='tertiary'>Контакты</Header>}>
-        {/*// @ts-ignore Типы не совместимы */}
-        <SimpleCell
-          before={
-            // @ts-ignore Типы не совместимы
-            <Avatar size={48} fallbackIcon={<Icon28Users />} src={winxAva} />
-          }
-          subtitle='Наша группа | Любые вопросы'
-        >
-          {/*// @ts-ignore Типы не совместимы */}
-          <Link target='_blank' href='https://vk.com/diary_spo'>
-            Дневник СПО
-          </Link>
-        </SimpleCell>
-        {/*// @ts-ignore Типы не совместимы */}
-        <SimpleCell
-          before={
-            // @ts-ignore Типы не совместимы
-            <Avatar size={48} fallbackIcon={<Icon28Users />} src={diaryAva} />
-          }
-          subtitle='Исходный код на GitHub'
-        >
-          {/*// @ts-ignore Типы не совместимы */}
-          <Link target='_blank' href='https://github.com/Diary-SPO'>
-            Diary SPO
-          </Link>
-        </SimpleCell>
-      </Group>
-
-      <Footer />
+      <Suspense id='Settings_FACF'>
+        <FAQ />
+        <Actions />
+        <Contacts />
+        <Footer />
+      </Suspense>
     </Panel>
   )
 }
