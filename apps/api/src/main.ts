c52a06e2b78276371047a07fb7df7335fffff1ea
Евгений Малинин <zamelane@vk.com>
2024-03-03 22:37:04 +0700

    Вынес часовую зону

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,16 @@
+import { TIMEZONE } from '@config'
 import { cors } from '@elysiajs/cors'
 import { swagger } from '@elysiajs/swagger'
 import routes from '@routes'
 import { Elysia } from 'elysia'
 import { compression } from 'elysia-compression'
 import { helmet } from 'elysia-helmet'
+import { getTimezone } from './config/getTimeZone'
 
+// настраиваем сервер...
 const port = Bun.env.PORT ?? 3003
 const app = new Elysia()
+  // документация...
   .use(
     swagger({
       path: '/documentation',
@@ -18,11 +22,13 @@ const app = new Elysia()
       }
     })
   )
+  // вырубаем корс...
   .use(
     cors({
       origin: true
     })
   )
+  // сжатие...
   .use(
     compression({
       type: 'gzip',
@@ -32,12 +38,20 @@ const app = new Elysia()
       encoding: 'utf-8'
     })
   )
+  // заголовки...
   .use(helmet())
   .use(routes)
   .listen(port)
 
 console.log(
   `Backend running at http://${app.server?.hostname}:${app.server?.port}`
+)
+
+// Соробщение о текущем часовом поясе
+console.log(
+  `Будет использоваться следующая часовая зона: '${TIMEZONE}'`
+  + (getTimezone() !== TIMEZONE ? ' (задана через .env)' : '')
+  + '.'
 )
 
 const workerURL = new URL('worker', import.meta.url).href
