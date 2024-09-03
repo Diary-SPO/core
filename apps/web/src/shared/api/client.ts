fbdbae92c0a00049232ac6daa9172b0018de448e
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2024-09-03 23:30:10 +0700

    fix: add header (#332)

--- apps/web/src/shared/api/client.ts
+++ apps/web/src/shared/api/client.ts
@@ -7,6 +7,11 @@ export const client = treaty<App>('http://localhost:3003', {
     const headers = new Headers()
 
     headers.append('secret', localStorage.getItem('token') || '')
+    /**
+     * @TODO: в будущем может потребоваться добавить другой тип (например, form-data).
+     * Надо чтобы хедер с типом контента не затирался, т.к. либа умеет его ставить сама
+     */
+    headers.append('Content-type', 'application/json')
     options.headers = headers
   }
 })
