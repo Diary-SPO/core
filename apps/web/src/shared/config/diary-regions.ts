0ce18d3c26a1477bf5817d5496622bf1fd3e3eb8
Zamelane <zamelane@vk.com>
2026-09-23 12:49:37 +0700

    Sort diary regions

--- apps/web/src/shared/config/diary-regions.ts
+++ apps/web/src/shared/config/diary-regions.ts
@@ -10,72 +10,72 @@ export const DEFAULT_DIARY_URL = 'https://poo.tomedu.ru'
  */
 export const DIARY_REGIONS: DiaryRegion[] = [
   {
-    name: 'Краснодарский край',
-    url: 'https://spo.rso23.ru'
-  },
-  {
-    name: 'Челябинская область',
-    url: 'https://poo.edu-74.ru'
-  },
-  {
-    name: 'Республика Саха (Якутия)',
-    url: 'https://poo.e-yakutia.ru'
-  },
-  {
-    name: 'Республика Бурятия',
-    url: 'https://spo.obr03.ru'
-  },
-  {
-    name: 'Приморский край',
-    url: 'https://poo.prim-edu.ru'
-  },
-  {
-    name: 'Карачаево-Черкесская Республика',
-    url: 'https://poo.kchgov.ru'
-  },
-  {
     name: 'Алтайский край',
     url: 'https://netspo.edu22.info'
-  },
-  {
-    name: 'Республика Коми',
-    url: 'https://giseo-spo.rkomi.ru'
-  },
-  {
-    name: 'Ямало-Ненецкий АО',
-    url: 'https://spo.yanao.ru'
   },
   {
     name: 'Волгоградская область',
     url: 'https://spo.volganet.ru'
   },
   {
-    name: 'Калужская область',
-    url: 'https://spo.admoblkaluga.ru'
-  },
-  {
     name: 'Забайкальский край',
     url: 'https://poo.zabedu.ru'
+  },
+  {
+    name: 'Калужская область',
+    url: 'https://spo.admoblkaluga.ru'
   },
   {
     name: 'Камчатский край',
     url: 'https://prof.sgo41.ru'
   },
   {
-    name: 'Самарская область',
-    url: 'https://spo.asurso.ru'
+    name: 'Карачаево-Черкесская Республика',
+    url: 'https://poo.kchgov.ru'
+  },
+  {
+    name: 'Краснодарский край',
+    url: 'https://spo.rso23.ru'
+  },
+  {
+    name: 'Приморский край',
+    url: 'https://poo.prim-edu.ru'
+  },
+  {
+    name: 'Республика Бурятия',
+    url: 'https://spo.obr03.ru'
   },
   {
     name: 'Республика Ингушетия',
     url: 'https://poo.edu-ri.ru'
   },
   {
-    name: 'Ульяновская область',
-    url: 'https://spo.cit73.ru'
+    name: 'Республика Коми',
+    url: 'https://giseo-spo.rkomi.ru'
+  },
+  {
+    name: 'Республика Саха (Якутия)',
+    url: 'https://poo.e-yakutia.ru'
+  },
+  {
+    name: 'Самарская область',
+    url: 'https://spo.asurso.ru'
   },
   {
     name: 'Томская область',
     url: DEFAULT_DIARY_URL
+  },
+  {
+    name: 'Ульяновская область',
+    url: 'https://spo.cit73.ru'
+  },
+  {
+    name: 'Челябинская область',
+    url: 'https://poo.edu-74.ru'
+  },
+  {
+    name: 'Ямало-Ненецкий АО',
+    url: 'https://spo.yanao.ru'
   }
 ]
 
