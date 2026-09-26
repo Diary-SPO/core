dd17ee997fa53e44474e7471835a02eaa44aa872
Zamelane <zamelane@vk.com>
2026-09-26 19:18:11 +0700

    Update imgs and add tg social

--- apps/web/src/pages/Settings/Contacts.tsx
+++ apps/web/src/pages/Settings/Contacts.tsx
@@ -1,17 +1,23 @@
 import { Icon28Users } from '@vkontakte/icons'
 import { Avatar, Group, Header, Link, SimpleCell } from '@vkontakte/vkui'
-import { diaryAva, winxAva } from '../../shared/config/images.ts'
+import { diaryAva, tgAva, vkAva } from '../../shared/config/images.ts'
 
 const Contacts = () => {
   return (
     <Group header={<Header size='m'>Контакты</Header>}>
       <SimpleCell
-        before={
-          <Avatar size={48} fallbackIcon={<Icon28Users />} src={winxAva} />
-        }
+        before={<Avatar size={48} fallbackIcon={<Icon28Users />} src={vkAva} />}
         subtitle='Поддержка и обратная связь'
       >
         <Link target='_blank' href='https://vk.com/diary_spo'>
+          Дневник СПО
+        </Link>
+      </SimpleCell>
+      <SimpleCell
+        before={<Avatar size={48} fallbackIcon={<Icon28Users />} src={tgAva} />}
+        subtitle='Поддержка и обратная связь'
+      >
+        <Link target='_blank' href='https://t.me/diary_spo'>
           Дневник СПО
         </Link>
       </SimpleCell>
--- apps/web/src/shared/config/images.ts
+++ apps/web/src/shared/config/images.ts
@@ -1,4 +1,6 @@
-import diaryAva from '../../assets/images/diary-ava.png'
+import diaryAva from '../../assets/images/diary-ava.webp'
+import tgAva from '../../assets/images/tg128.webp'
+import vkAva from '../../assets/images/vk128.webp'
 import winxAva from '../../assets/images/winx48.webp'
 
-export { winxAva, diaryAva }
+export { diaryAva, tgAva, vkAva, winxAva }
